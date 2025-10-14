b9e012da9e79240996490b00fe2c0149cdb00c5f
sandraelsa16 <sandraelsavarughese@gmail.com>
2025-10-14 23:00:45 +0530

    Initial commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets'
+
+const Main = () => {
+  return (
+   <>
+    <div className="main">
+        <div className="nav">
+            <p>Gemini</p>
+            <img src={assets.user_icon} alt="" />
+        </div>
+        <div className="main-container">
+            <div className="greet">
+                <p><span>Hello,Dev. </span></p>
+                <p>How can I help You Today?</p>
+            </div>
+            <div className="cards">
+                <div className="card">
+                    <p>Suggest beautiful places for the upcoming road trip</p>
+                    <img src={assets.compass_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Briefly summarize this concept : Urban Planning</p>
+                    <img src={assets.bulb_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Brainstrom team bonding activities for our work retreat</p>
+                    <img src={assets.message_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Improve the readability of the following code</p>
+                    <img src={assets.code_icon} alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
+
+   </>
+
+  )
+}
+
+export default Main
